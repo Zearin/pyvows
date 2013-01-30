10acaec018cee2a057797d3a6e82ae6fee51ba78
Zearin <zearin@gonk.net>
2013-01-30 14:23:12 -0500

    Created placeholder file for issue #67
    If possible, please add tests to this file that can squash issue 67

---
+++ tests/bugs/67_vows.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Tests:
+    https://github.com/heynemann/pyvows/issues/67
+
+'''
+# pyvows testing engine
+# https://github.com/heynemann/pyvows
+
+# This file is MIT licensed
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 nathan dotz
+
+from pyvows import Vows, expect
+
+# FIXME:    make sure this bug is squashed!
+#
+#           https://github.com/heynemann/pyvows/issues/67
